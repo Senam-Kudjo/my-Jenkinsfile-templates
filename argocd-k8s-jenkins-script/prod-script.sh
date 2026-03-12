0f4b55ea3dd12b1d3395c236c956ed845a3d9873
Senam Kudjo Humado <108673986+Senam-Kudjo@users.noreply.github.com>
2026-03-12 23:21:52 +0000

    Add production script for ArgoCD and Kubernetes

---
+++ argocd-k8s-jenkins-script/prod-script.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+# Check if app_name parameter is provided
+if [ -z "$1" ]; then
+    echo -e "${RED}Error: Application name parameter is required.${NC}"
+    echo "Usage: $0 <app_name>"
+    exit 1
+fi
+
+function create() {
+    local app_name="$1"  # Accept app_name as a function parameter
+    local image_name="$2" # Accept image_name as a function parameter
+    local PORTINGO="$3"
+
+    cp template-application.yaml production/applications/"${app_name}".yaml
+    cd production/applications/
+
+    # If the file has been created
+    if [ -f "${app_name}.yaml" ]; then
+        echo -e "${GREEN} ${app_name}.yaml has been created.${NC}"
+
+        #### Update ARGOCD applications manifest
+        sed -i "s/name: app_name/name: ${app_name}/" "${app_name}.yaml"
+        grep name "${app_name}.yaml"
+
+        sed -i "s#path: production/manifests/app_name#path: production/manifests/${app_name}#" "${app_name}.yaml"
+        grep path "${app_name}.yaml"
+        #### End of ARGOCD applications manifest
+
+        #### Update k8s manifest
+        cd ../manifests
+        mkdir "${app_name}"
+        cd "${app_name}"
+        cp ../template-object.yaml "${app_name}.yaml"
+        sed -i "s/app_name/${app_name}/" "${app_name}.yaml"
+        sed -i "s/reimage/${image_name}/" "${app_name}.yaml"
+        sed -i "s/PORTINGO/${PORTINGO}/" "${app_name}.yaml"
+
+    else
+        # If file does not exist
+        echo -e "${RED} file does not exist.${NC}"
+    fi
+}
+
+# Call the function with the command-line parameter
+create "$1" "$2" "$3"
